00b9c7d3b3a709ca6f6388d29788a6c0e4cda6ca
vanthu <vanthu@localhost>
2023-07-19 16:56:44 +0700

    Login store front

--- Test Suites/TS003_LoginStoreFront.ts
+++ Test Suites/TS003_LoginStoreFront.ts
@@ -5,10 +5,35 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>1cef6d13-3ec8-4685-a349-b83af7e8e8e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>e14289a7-a62d-42a0-8d21-761f6f3476e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004_Login_Storefront</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b5022e92-1fcb-47c0-9a9e-85443dad8813</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7e5c96d4-8747-40bf-8353-f1cf87a07067</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a3b01c46-d537-4f4f-8f4c-92458c4f94d3</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
